9a3e2946407d5e74164010d80c8c3a9d6eb39222
romkravets <romann.kravets@gmail.com>
2019-10-20 22:27:50 +0300

    Added onChange

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import Header from './Header.js'
 import ToDo from './ToDo.js'
 import './App.css';
 
-function App() {
+class App extends React.Component {
   state = {todos: ['first','second','third']};
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Header title={"This is our ToDo"}/>
-        <ToDo todoList={this.state.todos}/>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Header title={"This is our ToDo"}/>
+          <ToDo todoList={this.state.todos}/>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 export default class Header extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {inputValue: ''};
+   }
+
+   onChange = event => this.setState({inputValue: event.target.value})
+
    render() {
       return (
-         <div className="header">{this.props.title}</div>
+         <Fragment>
+               <div className="header">{this.props.title}</div>
+               <div>
+                  <input value={this.state.inputValue} onChange={this.onChange}/>
+                  <button>Add</button>
+               </div>
+         </Fragment>
       )
    }
 }
